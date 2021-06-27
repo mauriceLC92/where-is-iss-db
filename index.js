1a086fa3a18963915476d7052895e5031d2f897a
Maurice Le Cordier <mauricelecordier@gmail.com>
2021-06-27 15:56:13 +0200

    added barebones nodejs

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 require('dotenv').config();
-
-console.log("Hello!")
+const  http = require('http'),
+ 
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || 8080;
+ 
+// create a simple server
+let server = http.createServer(function (req, res) {
+ 
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+ 
+    });
+ 
+// listen on the port
+server.listen(port, function () {
+ 
+    console.log('app up on port: ' + port);
+ 
+});
